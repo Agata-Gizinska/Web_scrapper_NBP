79f62bd791168d66c300bb750a3e5126184e5c05
Agata Gizińska <gizinska.agata@gmail.com>
2022-05-18 15:31:10 +0200

    Updated tests, updated .gitignore, added html_text.txt

--- test_webscrap_currency_rate.py
+++ test_webscrap_currency_rate.py
@@ -1,6 +1,7 @@
 import pytest
 import requests
-from webscrap_currency_rate import get_currency, Webscrapper
+from webscrap_currency_rate import (get_currency, 
+                                    Webscrapper, get_url, get_currency)
 
 
 def test_request_status(website):
@@ -11,8 +12,15 @@ def test_webscrapper_currency_short(ws_instance):
     assert list(ws_instance.currency.keys()) == ['USD', 'EUR', 'CHF', 'GBP', 'JPY']
 
 
-def test_get_html_components_and_prepare_data():
-    pass
+def test_get_html_components_and_prepare_data(ws_instance):
+    with open('html_text.txt') as mock_html:
+        ws_instance.get_html_components_and_prepare_data(mock_html)
+        assert type(ws_instance.headings) == list
+        assert type(ws_instance.content) == list
+        assert ws_instance.headings[0] == 'Nazwa waluty'
+        assert ws_instance.content[1][0] == 'euro'
+        assert ws_instance.content[2][1] == '1 CHF'
+        assert type(ws_instance.content[2][2]) == str
 
 
 def test_mock_data():
